2a081f9695eadae711246d6d15f549f03640db9f
Nodir <yusupovn948@gmail.com>
2023-06-26 03:17:20 +0500

    commit

--- Hard1.cmd/main.go
+++ Hard1.cmd/main.go
@@ -1,19 +1,61 @@
 package main
 
 
-import "lesson/Hard"
-
+import (
+	//"fmt"
+	"lesson/Hard"
+)
 func main()  {
-	// flight:=Hard.Flight{
-	// 	FlightNumber: "A213",
-	// 	Source: "New-york",
-	// 	Destination: "America",
-	// 	Passengers: []string{"John"},
+	// mijoz1:=Hard.User {
+	// 	Ism: "Nodir",
+	// 	Email: "Nodirqurbonbayev3@gmail.com",
+	// 	Savatcha: []Hard.MahsulotTuri{},
 	// }
-	// flight.AddPassenger("John")
-	// flight.DisplayPassenger()
-	// flight.RemovePassenger("Jek")
-	// flight.DisplayPassenger()
-
-
+	// mijoz2:=Hard.User{
+	// 	Ism: "John",
+	// 	Email: "John@gmail.com",
+	// 	Savatcha: []Hard.MahsulotTuri{},
+	// }
+	// sut:=Hard.MahsulotTuri{
+	// 	Name: "Milk",
+	// 	Price: 15000.0,
+	// }
+	// non:=Hard.MahsulotTuri{
+	// 	Name: "Non",
+	// 	Price: 12000.0,
+	// }
+	// Chears:=Hard.MahsulotTuri{
+	// 	Name: "Lays",
+	// 	Price: 18000.0,
+	// }
+	// mijoz1.AddMahsulot(sut)
+	// mijoz1.AddMahsulot(non)
+	// mijoz1.AddMahsulot(Chears)
+	// fmt.Println(mijoz1)
+	// mijoz2.AddMahsulot(Chears)
+	// mijoz2.AddMahsulot(sut)
+	// mijoz2.AddMahsulot(non)
+	// mijoz1.OchirMahsulot(Chears)
+	// fmt.Println(mijoz2)
+	nodir:=Hard.Users{
+		Name: "Nodir",
+		Email: "Nodir@gmail.com",
+		Friend: []Hard.Users{},
+	}
+	john:=Hard.Users{
+		Name: "John",
+		Email: "John@gmail.com",
+		Friend: []Hard.Users{},
+	}
+	Parker:=Hard.Users{
+		Name: "Parker",
+		Email: "parker@gmail.com",
+		Friend: []Hard.Users{},
+	}
+	nodir.AddFriend(john)
+	Parker.AddFriend(nodir)
+	john.AddFriend(Parker)
+	john.AddFriend(nodir)
+	john.AddFriend(john)
+	john.Display()
 }
